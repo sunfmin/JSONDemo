5577e902532e66dfbbcc79e05885095435c4876b
Felix Sun <sunfmin@gmail.com>
2013-04-12 17:41:35 +0800

    Initial Commit

---
+++ JSONDemo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  JSONDemo
+//
+//  Created by Felix Sun on 4/12/13.
+//  Copyright (c) 2013 Felix Sun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
